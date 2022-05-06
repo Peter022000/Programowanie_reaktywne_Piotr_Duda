2646bac1542570cd76dda9c77878f933c0af63be
Piotr Duda <57139943+Peter022000@users.noreply.github.com>
2022-05-06 11:02:58 +0200

    Lab 9

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './App.css';
+import Posts from "./components/posts";
+
+function App() {
+  return (
+      <main className="container">
+        <Posts/>
+      </main>
+  );
+}
+
+
+export default App;
